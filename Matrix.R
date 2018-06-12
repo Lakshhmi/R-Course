b26b36a2706d8a037b3af231daeb9d338449597b
Lakshmi <Lakshhmi@gmail.com>
2018-06-11 20:19:47 -0400

    Matrix

--- Matrix.R
+++ Matrix.R
@@ -61,3 +61,51 @@ mat * mat
 #Actual matrix multiplication
 
 mat %*% mat
+
+
+stock.prices <- c(23,27,23,21,34)
+stock.prices
+
+names(stock.prices) <- c("Mon","Tue","Wed","Thu","Fri")
+
+mean(stock.prices)
+
+over.23 <- stock.prices>23
+
+stock.prices[over.23]
+
+
+max(stock.prices)
+
+max.prices <- stock.prices==max(stock.prices)
+max.prices
+
+stock.prices[max.prices]
+
+#Matrix Operations
+colSums(stocks.matrix)
+
+rowSums(stocks.matrix)
+
+rowMeans(stocks.matrix)
+
+FB <- c(110,111,112,120,125)
+tech.stocks <- rbind(stocks.matrix,FB)
+tech.stocks
+
+avg <- rowMeans(tech.stocks)
+avg
+
+tech.stocks <- cbind(tech.stocks,avg)
+tech.stocks
+
+#Matrix selection and indexing
+
+mat <- matrix(1:50, byrow = TRUE, nrow = 5, ncol = 10)
+mat
+
+mat[1,]
+
+mat[,1]
+
+mat[1:3,]
