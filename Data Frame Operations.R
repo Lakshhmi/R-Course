1e08574046d5be1135a9a8e5ae08f82c8bda61db
Lakshmi <Lakshhmi@gmail.com>
2018-06-26 07:26:33 -0400

    Data Frame Operations

--- Data Frame Operations.R
+++ Data Frame Operations.R
@@ -1,5 +1,7 @@
 # Data Frame Operations
 
+# Creating data frames
+
 empty <- data.frame() # It creates empty data frame
 empty
 
@@ -18,17 +20,20 @@ c2
 df <- data.frame(col.name.1 = c1, col.name.2 = c2)
 df
 
+# Importing and Exporting data
+
 write.csv(df,file = "saved_df.csv")
 read.csv('saved_df.csv')
 
-
+# Getting information about dataframe
 df
 nrow(df)
 ncol(df)
 colnames(df)
 rownames(df)
 
-summary(df)
-str(df)
+summary(df) # statistcal summary of the data frame
+
+str(df) # returns the structure of the data frame
 
 
