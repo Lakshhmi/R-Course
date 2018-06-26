334788fb3c28f78fac81162a00bcff862e7e0c73
Lakshmi <Lakshhmi@gmail.com>
2018-06-26 17:01:33 -0400

    Data Frame Exercises

--- Data Frame Operations.R
+++ Data Frame Operations.R
@@ -1,6 +1,6 @@
 # Data Frame Operations
 
-# Creating data frames
+# 1. Creating data frames
 
 empty <- data.frame() # It creates empty data frame
 empty
@@ -20,12 +20,12 @@ c2
 df <- data.frame(col.name.1 = c1, col.name.2 = c2)
 df
 
-# Importing and Exporting data
+# 2. Importing and Exporting data
 
 write.csv(df,file = "saved_df.csv")
 read.csv('saved_df.csv')
 
-# Getting information about dataframe
+# 3. Getting information about dataframe
 df
 nrow(df)
 ncol(df)
@@ -35,5 +35,101 @@ rownames(df)
 summary(df) # statistcal summary of the data frame
 
 str(df) # returns the structure of the data frame
+
+
+# 4. Referencing Cells
+
+df[[6,2]] # Call single cell value
+
+
+df[[5,'col.name.2']]
+
+# Call single cell and assign a value
+df[[2,'col.name.1']] <- 99
+df
+
+# 5. Referencing Rows
+
+df[1,]
+
+# 6. Referencing Columns
+
+mtcars # Built-in Data frame in R
+str(mtcars)       # returns structure
+colnames(mtcars)  # column names
+head(mtcars)      # first 6 rows
+tail(mtcars)      # last 6 rows
+dim(mtcars)       # get number of rows and columns
+
+# 4 different ways to get a vector of columns
+mtcars[,'drat']
+mtcars$drat
+mtcars[,5]
+mtcars[['drat']]  # returns vector
+mtcars['drat']    # returns data frame
+
+mtcars[c('mpg','drat')] # Should pass a vector to get multiple columns
+
+head(mtcars[c('mpg','drat')])
+
+head(mtcars,3) # get first 3 rows from the data frame
+
+sapply(mtcars,mean,na.rm=TRUE) # get mean value
+
+# Adding new row
+
+df2 <- data.frame(col.name.1=2000,col.name.2 = 'new')
+df2
+
+dfnew <- rbind(df,df2)
+dfnew 
+
+
+df$newcol <- 2*df$col.name.1
+df
+
+# Setting column names
+
+colnames(df)
+colnames(df) <- c("One","Two",'Three') # rename all the column name at once
+df
+
+
+colnames(df)[1] <- "New One"
+df
+
+
+# Selecting multiple rows
+
+df[2:7,2]
+
+
+df[-2,] # Select all rows but not 2nd
+
+# Selecting rows with conditions
+
+mtcars[mtcars$mpg >30,]
+mtcars[mtcars$mpg > 30 & mtcars$hp >65,]
+
+
+mtcars[ (mtcars$mpg >25) & (mtcars$cyl == 4), c('mpg','hp','cyl','gear')]
+
+
+subset(mtcars, mpg >30 & hp > 65)
+
+# Selecting multiple columns
+
+mtcars[,c(1,2,3)]
+mtcars[,c('mpg','hp','cyl')]
+mtcars[,c('mpg','hp','cyl')][1:7,]
+
+# Dealing with missing data
+
+is.na(mtcars)
+any(is.na(df))
+any(is.na(mtcars))
+
+any(is.na(mtcars$mpg))
+mtcars$mpg[is.na(mtcars$mpg)] <- mean(mtcars$mpg)
 
 
---
+++ DataFrame Exercises.R
@@ -0,0 +1,42 @@
+# Data Frame Exercises
+
+#Age,Weight,Sex
+
+v1 <- c('Sam',22,150,'M')
+
+v1
+Name <- c('Sam','Frank','Amy')
+Age <- c(22,25,26)
+Weight <- c(150,165,120)
+Sex <- c('M','M','F')
+
+dfPerson <- data.frame(Name,Age,Weight,Sex)
+dfPerson[c('Age','Weight','Sex')]
+
+is.data.frame(mtcars)
+
+
+mat <- matrix(1:25,nrow = 5)
+as.data.frame(mat)
+
+
+df <- mtcars
+df
+
+head(df)
+
+mean(df$mpg)
+
+df[df$cyl == 6,]
+
+df[c('am','gear','carb')]
+
+df$performance <- df$hp/df$wt
+
+head(df)
+
+df$performance <- round(df$performance,digits = 2)
+
+head(df,3)
+
+df[(df$hp >100) & (df$wt >2.5),]
