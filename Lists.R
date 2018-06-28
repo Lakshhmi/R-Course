4ece0b30e602de30c92a051092e75046d2c4fb65
Lakshmi <Lakshhmi@gmail.com>
2018-06-28 13:42:51 -0400

    Lists

---
+++ Lists.R
@@ -0,0 +1,32 @@
+v <- c(1,2,3)
+m <- matrix(1:10,nrow = 2)
+df <- mtcars
+
+class(v)
+
+class(m)
+
+class(df)
+
+mylist <- list(v,m)
+mylist
+
+mynamedlist <- list(Vec = v, Mat = m)
+mynamedlist
+
+
+mynamedlist$Mat # or mynamedlist[[2]]
+
+mylist[1]
+mynamedlist[1]
+
+mynamedlist['Mat']
+
+class(mynamedlist$Mat) 
+
+class(mynamedlist['Mat'])
+
+double.list <- list(mynamedlist,mynamedlist)
+double.list
+
+str(mynamedlist)
