2fc468a15fa76a5d67190e26748c81151ecd5874
Lakshmi <Lakshhmi@gmail.com>
2018-07-02 18:21:15 -0400

    CSV Files.R

--- CSV Files.R
+++ CSV Files.R
@@ -10,3 +10,11 @@ class(ex)
 
 
 
+install.packages("readxl")
+
+library(readxl)
+excel_sheets('Sample-Data.xlsx')
+
+df <- read_excel('Sample-Data.xlsx',sheet = "Sheet1")
+
+entire.workbook <- lapply(excel_sheets('Sample-Data.xlsx'),read_excel,path="Sample-Data.xlsx")
