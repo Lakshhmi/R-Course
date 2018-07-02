24e8a360e68108551273ff2a0283b95ea9e92e76
Lakshmi <Lakshhmi@gmail.com>
2018-07-02 19:01:09 -0400

    CSV Files

--- CSV Files.R
+++ CSV Files.R
@@ -8,7 +8,7 @@ tail(ex)
 
 class(ex)
 
-
+##Excel with R
 
 install.packages("readxl")
 
@@ -18,3 +18,11 @@ excel_sheets('Sample-Data.xlsx')
 df <- read_excel('Sample-Data.xlsx',sheet = "Sheet1")
 
 entire.workbook <- lapply(excel_sheets('Sample-Data.xlsx'),read_excel,path="Sample-Data.xlsx")
+
+install.packages('xlsx')
+
+library(xlsx)
+
+head(mtcars)
+
+write.xlsx(mtcars,"output-example.xlsx")
