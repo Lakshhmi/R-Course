b8a77d3cd4fd315eaa212844240abf29725464ab
Lakshmi <Lakshhmi@gmail.com>
2018-07-04 12:19:29 -0400

    Basic R Programming

--- Basic R Program.R
+++ Basic R Program.R
@@ -15,3 +15,37 @@ df <- mtcars
 head(df)
 df$mpg > 20
 df[df$mpg > 20,]
+
+df[df$mpg > 20, 'mpg']
+
+df[df$mpg > 20, c('mpg','drat')]
+
+subset(df, mpg>20)
+
+subset(df, mpg>20,c('mpg','hp'))
+
+df[(df$mpg >20) & (df$hp > 100),]
+
+
+# If, else and elseif statement
+
+x<-13
+if (x==10){
+  print('x is equal to 10')
+}else if(x==12){
+  print('x is equal to 12')
+    }else
+      print('x is not equal to 10 or 12')
+
+# Conditional Statement Exercises
+x <- 13
+if (x%%2 == 0){paste(x , 'is a Even Number')}else
+  paste(x, 'is a Odd Number')
+
+
+x <- matrix(1:25,nrow = 5,ncol = 5,byrow = T)
+x
+
+if (is.matrix(x) == T){
+  print('Is a Matrix')
+}else print('Is not a Matrix')
