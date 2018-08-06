5e2bfd8f2cfde4c5b0ae391e0ca503c4b23711bf
Lakshmi <Lakshhmi@gmail.com>
2018-08-06 18:31:42 -0400

    Data Manipulation

--- Data Manipulation.R
+++ Data Manipulation.R
@@ -11,3 +11,11 @@ head(flights)
 
 summary(flights)
 
+# Dplyr built in functions
+# filter() and slice()
+# arrange()
+# select() and rename()
+# distinct()
+# mutate() and transmute()
+# summarise()
+# sample_n() and sample_frac()
