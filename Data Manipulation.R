57a4d80c306968868d8f4491ebe0451b34519cff
Lakshmi <Lakshhmi@gmail.com>
2018-10-26 10:33:23 -0400

    Data Manipulation

--- Data Manipulation.R
+++ Data Manipulation.R
@@ -83,4 +83,15 @@ install.packages('tidyr',repos = 'http://cran.us.r-project.org')
 library(tidyr)
 library(data.table)
 
+#Let's create some fake data that needs to be cleaned using tidyr
 
+comp <- c(1,1,1,2,2,2,3,3,3)
+yr <- c(1998,1999,2000,1998,1999,2000,1998,1999,2000)
+q1 <- runif(9, min=0, max=100)
+q2 <- runif(9, min=0, max=100)
+q3 <- runif(9, min=0, max=100)
+q4 <- runif(9, min=0, max=100)
+
+df <- data.frame(comp=comp,year=yr,Qtr1 = q1,Qtr2 = q2,Qtr3 = q3,Qtr4 = q4)
+
+df
