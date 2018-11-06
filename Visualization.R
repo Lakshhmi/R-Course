36fbd8a93bb9d0cf3a270e00fd996a8478321757
Lakshmi <Lakshhmi@gmail.com>
2018-11-06 12:01:44 -0500

    Visualization

--- Visualization.R
+++ Visualization.R
@@ -1,2 +1,8 @@
 install.packages('ggplot2')
 library(ggplot2)
+library(movies)
+df <- movies <- movies[sample(nrow(movies), 1000), ]
+
+head(mtcars)
+
+df <- mtcars <- mtcars[sample(nrow(mtcars), 1000), ]
