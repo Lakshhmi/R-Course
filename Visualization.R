ac247341c749308e06c04068236818dd862d5266
Lakshmi <Lakshhmi@gmail.com>
2018-11-08 10:45:27 -0500

    Visualization

--- Visualization.R
+++ Visualization.R
@@ -20,3 +20,11 @@ pl + geom_bin2d()
 
 # Control bin sizes
 pl + geom_bin2d(binwidth=c(2,1))
+
+#2D Density Plot
+pl + geom_density2d()
+
+#2d Hex Plot
+pl + geom_hex()
+
+pl + geom_hex() + scale_fill_gradient(high='red',low='blue')
