fc7abb179f170d383cdd942a737da5ead2ed83d9
Lakshmi <Lakshhmi@gmail.com>
2018-11-07 21:01:55 -0500

    Visualization

--- Visualization.R
+++ Visualization.R
@@ -6,3 +6,8 @@ df <- movies <- movies[sample(nrow(movies), 1000), ]
 head(mtcars)
 
 df <- mtcars <- mtcars[sample(nrow(mtcars), 1000), ]
+
+library(ggplot2)
+df <- movies
+
+qplot(x=year, y=rating, data = df, geom = "density2d")
